261f00a9d4446feec9c03deb0d4ae3e642628ce1
alanfoandrade <alanfrank@gec.inatel.br>
2019-10-08 22:19:17 -0300

    ultimo desafio

--- modulo4/desafio.js
+++ modulo4/desafio.js
@@ -8,8 +8,66 @@ function checaIdade(idade) {
 
 checaIdade(20)
   .then(function() {
-    console.log('Maior que 18');
+    console.log("Maior que 18");
   })
   .catch(function() {
-    console.log('Menor que 18');
+    console.log("Menor que 18");
   });
+
+var listElement = document.querySelector("ul");
+var btnElement = document.querySelector("button.botao");
+
+btnElement.onclick = () => {
+  listRepositories();
+};
+
+function renderRepositories(repositories) {
+  listElement.innerHTML = "";
+
+  for (repo of repositories) {
+    const liElement = document.createElement("li");
+    const textElement = document.createTextNode(repo.name);
+
+    liElement.appendChild(textElement);
+    listElement.appendChild(liElement);
+  }
+}
+
+function renderLoading() {
+  listElement.innerHTML = "";
+
+  const loadingElement = document.createElement("li");
+  const textElement = document.createTextNode("Carregando...");
+
+  loadingElement.appendChild(textElement);
+  listElement.appendChild(loadingElement);
+}
+
+function renderError() {
+  listElement.innerHTML = "";
+
+  const errorElement = document.createElement("li");
+  const textElement = document.createTextNode("ERRO!");
+
+  errorElement.style.color = "F00";
+
+  errorElement.appendChild(textElement);
+  listElement.appendChild(errorElement);
+}
+
+function listRepositories() {
+  var user = document.querySelector("#user").value;
+
+  if (!user) return;
+
+  renderLoading();
+
+  axios
+    .get(`https://api.github.com/users/${user}/repos`)
+    .then(response => {
+      renderRepositories(response.data);
+    })
+    .catch(error => {
+      renderError();
+    });
+}
